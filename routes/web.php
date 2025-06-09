7a5c3b33d431fed51d5ef25308966158e025290c
Meghamalu <196megha@gmail.com>
2025-06-09 12:32:22 +0530

    Initial commit

--- routes/web.php
+++ routes/web.php
@@ -9,5 +9,4 @@ Route::get('/checkout/{method}', [StripePaymentController::class, 'checkout']);
 Route::get('/success', [StripePaymentController::class, 'success'])->name('payment.success');
 Route::get('/cancel', [StripePaymentController::class, 'cancel'])->name('payment.cancel');
 
-// Add Webhook Route for Stripe
-Route::post('/stripe/webhook', [StripeWebhookController::class, 'handleWebhook']);
+
